ba4b4954794e7ddaab910b2133fe9c4e0e6074a9
vicfriedman <victoria.m.friedman@gmail.com>
2014-05-07 17:31:19 -0400

    added test for sleeping

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -8,6 +8,13 @@ describe 'LOOPS' do
     end
   end
 
+  describe '#countdown_with_sleep' do
+    it 'should take at least 5 seconds to execute' do
+      runtime = Benchmark.measure { countdown_with_sleep(5) }
+      expect(runtime.real.to_i >= 5).to be true
+    end
+  end
+
 end
 
 
