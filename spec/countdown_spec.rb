023de349299ac0b5bd8bd01076c53e7a7877b069
vicfriedman <victoria.m.friedman@gmail.com>
2014-05-07 17:22:55 -0400

    solutions

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -1,10 +1,18 @@
 require_relative './spec_helper'
+require 'benchmark'
 
 describe 'LOOPS' do
 
   describe '#countdown' do
     it 'returns HAPPY NEW YEAR!' do
       expect(countdown(12)).to eq "0 left. HAPPY NEW YEAR!"
+    end
+  end
+
+  describe '#countdown_with_sleep' do
+    it 'should take at least 5 seconds to execute' do
+      runtime = Benchmark.measure { countdown_with_sleep(5) }
+      expect(runtime.real.to_i >= 5).to be true
     end
   end
 
