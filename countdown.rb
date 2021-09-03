0dbf3501c52f08b42328490a87c9d44803f7c8f9
Rguardado1009 <rguardado1009@gmail.com>
2021-09-03 11:20:30 -0400

    trying

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(int)
+int = -11
+    while int < 1   
+        int += 1
+        puts "#{int} SECONDS(S)!"
+    end
+    puts "HAPPY NEW YEAR!"
 end
