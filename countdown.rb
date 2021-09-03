0e43f4651e1b16e7ad82af03ef13d779f065a246
Rguardado1009 <rguardado1009@gmail.com>
2021-09-03 11:21:58 -0400

    commit tree 2435ac3695a585c0bf27d1ece0cc9adb53720b43

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
 
 def countdown(int)
-int = -11
-    while int < 1   
-        int += 1
+    while int > 0   
+        int -= 1
         puts "#{int} SECONDS(S)!"
     end
     puts "HAPPY NEW YEAR!"
