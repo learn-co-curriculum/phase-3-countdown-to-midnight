cc2d19d485f75a92197afb658942e8c9d818c68b
Daryl Rogony <darylrogony@gmail.com>
2022-06-09 13:26:33 +0300

    commit tree c107db0289c810324aae0212976ace8ddf1317f6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown time
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep time
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    sleep(1)
+    time -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
