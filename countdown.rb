245b8cf4b3c760daf852f649489b6682e9ece4e5
Cecilia Milla <cmilla1020@gmail.com>
2022-12-05 18:04:47 -0500

    commit tree 1e89e218ecbc039c9922243e3b8305845f061013

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown x
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+    while x > 0
+        sleep(1)
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+countdown 10
+countdown 12
+countdown_with_sleep 5
